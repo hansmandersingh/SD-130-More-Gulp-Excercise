901cc540ee1b14122cea8c498137fbf37d58a0fc
Hans <hansmandersingh1998@gmail.com>
2020-05-15 13:03:13 -0500

    created new gulpfile to run the dependencies

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+function cleanUpCss() {
+  return gulp.src('src/css/*')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css'))
+}
+
+function imageCompress() {
+  return gulp.src('src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+}
+
+function babel() {
+  return gulp.src('src/js/app.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+}
+
+exports.default = gulp.parallel(cleanUpCss, imageCompress, babel);
